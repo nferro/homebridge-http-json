715aac48ee313da67c036b1acd0258eee3a716fc
duchenpaul <duchenpaul@gmail.com>
2019-10-31 23:53:23 +0800

    enhance
    Fixed bugs that cannot read sensors except `themostat` and one characteristic limit, now it can read all kind of json sensors with multiple characteristics, but the config structure is changed.

--- index.js
+++ index.js
@@ -23,13 +23,14 @@ HttpAccessory.prototype = {
         var informationService = new Service.AccessoryInformation();
 
         informationService
-            .setCharacteristic(Characteristic.Manufacturer, "Nuno Ferro")
+            .setCharacteristic(Characteristic.Manufacturer, "Chenny Du")
             .setCharacteristic(Characteristic.Model, "HTTP JSON")
-            .setCharacteristic(Characteristic.SerialNumber, "ACME#1")
+            .setCharacteristic(Characteristic.SerialNumber, "whatTheFuck?")
 
         this.log("Load Service: " + this.service)
         var services = [informationService];
 
+        // console.log(this.sensors)
         for (var i = this.sensors.length - 1; i >= 0; i--) {
             let sensor = this.sensors[i];
             let url = this.url;
@@ -38,17 +39,32 @@ HttpAccessory.prototype = {
             this.log("service: " + sensor.service)
             this.log("name: " + sensor.name)
             newService = new Service[sensor.service](sensor.name);
-            newService.getCharacteristic(Characteristic[sensor.characteristic])
-                .on('get', function(callback) {
-                    console.log(sensor.name + " Triggered");
-                    superagent.get(url).end(function(err, res) {
-                        if (res && res.body[sensor.field]) {
-                            callback(null, res.body[sensor.field]);
-                        } else {
-                            callback(null, null);
-                        }
-                    });
-                })
+            for (var x = sensor.characteristics.length - 1; x >= 0; x--) {
+                console.log("Loading: " + sensor.characteristics[x].characteristic);
+                // console.log(sensor.characteristics[x]);
+
+                let characteristic = sensor.characteristics[x].characteristic;
+                newService.getCharacteristic(Characteristic[sensor.characteristics[x].characteristic])
+                    .on('get', function(callback) {
+                        console.log(sensor.name + " Triggered");
+                        // console.log(sensor);
+                        // console.log(sensor.characteristics[i].characteristic)
+
+                        superagent.get(url).end(function(err, res) {
+                            let characteristicField = sensor.characteristics.filter(function(y) {return y.characteristic === characteristic;})[0].field
+                            // console.log("Get characteristic: " + characteristic);
+                            // console.log("Get value: " + characteristicField);
+
+                            if (res && res.body[characteristicField]) {
+                                callback(null, res.body[characteristicField]);
+                            } else {
+                                callback(null, null);
+                            }
+
+                        });
+                    })
+
+            }
             services.push(newService);
         }
 
